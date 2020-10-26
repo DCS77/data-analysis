bc55ad02f49e1ffe397c6517f9a3b09a1057fa6c
Daniel Smit <dcsmit7@gmail.com>
2020-10-27 00:22:24 +1030

    Add first stage of data cleaner

---
+++ data_store/datacleaner.py
@@ -0,0 +1,17 @@
+import csv
+
+class DataCleaner:
+  def __init__(self):
+    return
+
+  # Remove rows which have less or more columns than expected
+  def remove_long_or_short_rows(self, input_csv_file, output_csv_file, num_cols):
+    with open(output_csv_file, 'w') as write_file:
+      writer = csv.writer(write_file, delimiter=',')
+ 
+      with open(input_csv_file, 'r') as read_file:
+        reader = csv.reader(read_file, delimiter=',')
+
+        for row in reader:
+          if len(row) is num_cols:
+            writer.writerow(row)
